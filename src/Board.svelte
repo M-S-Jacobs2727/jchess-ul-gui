6b14e39570835f487ba08455c46b289c8c4dd848
Michael Jacobs <M.S.Jacobs2727@gmail.com>
2024-02-14 19:14:02 -0500

    set up basic app layout

---
+++ src/Board.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	const rows = [1, 2, 3, 4, 5, 6, 7, 8];
+	const cols = [1, 2, 3, 4, 5, 6, 7, 8];
+	function isWhite(row: number, col: number) {
+		return (row + col) % 2 == 0;
+	}
+</script>
+
+<div class="board">
+	{#each rows as row}
+		{#each cols as col}
+			{#if isWhite(row, col)}
+				<div class="whiteSquare"></div>
+			{:else}
+				<div class="blackSquare"></div>
+			{/if}
+		{/each}
+	{/each}
+</div>
+
+<style>
+	.whiteSquare {
+		background-image: url('static/whiteSquare.svg');
+	}
+	.blackSquare {
+		background-image: url('static/blackSquare.svg');
+	}
+	.board {
+		display: grid;
+		aspect-ratio: 1 / 1;
+		grid-template-columns: repeat(8, 1fr);
+		grid-template-rows: repeat(8, 1fr);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Board from '../Board.svelte';
+</script>
+
+<div class="screen">
+	<div class="board-area">
+		<Board />
+	</div>
+	<div class="eval-area"></div>
+	<div class="move-area"></div>
+</div>
+
+<style>
+	.screen {
+		display: grid;
+		inset: 1fr;
+		width: 100%;
+		height: 100%;
+
+		grid-template-areas:
+			'b b b e e'
+			'b b b e e'
+			'b b b e e'
+			'b b b e e'
+			'm m m m m';
+		grid-template-columns: 3fr 2fr;
+		grid-template-rows: 4fr 1fr;
+	}
+	.board-area {
+		grid-area: 'b';
+	}
+	.eval-area {
+		grid-area: 'e';
+	}
+	.move-area {
+		grid-area: 'm';
+	}
+</style>
